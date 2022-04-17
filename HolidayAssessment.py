907538d15038b2776a9dbd7eca9821e19a45cfad
NickKartschoke <Nkartschoke@dev-10.com>
2022-04-17 17:03:14 -0500

    add

--- HolidayAssessment.py
+++ HolidayAssessment.py
@@ -1,41 +1,36 @@
 import datetime
 import json
+import os
 from bs4 import BeautifulSoup
 import requests
 from dataclasses import dataclass
 
 
-# -------------------------------------------
-# Modify the holiday class to 
-# 1. Only accept Datetime objects for date.
-# 2. You may need to add additional functions
-# 3. You may drop the init if you are using @dataclasses
-# --------------------------------------------
 @dataclass
 class Holiday:
-      
-    def __init__(self, name, date):
-        name: str
-        date: str 
-        #Your Code Here        
+    
+    name: str
+    date: datetime.datetime
     
     def __str__ (self):
-        return self.name, self.date
-        # String output
-        # Holiday output when printed.
-          
-           
-# -------------------------------------------
-# The HolidayList class acts as a wrapper and container
-# For the list of holidays
-# Each method has pseudo-code instructions
-# --------------------------------------------
+        return f"{self.name} ({self.date})"
+                   
 class HolidayList:
     def __init__(self):
-       self.innerHolidays = []
+        self.innerHolidays = []
+           
+    #Add a holiday 
+    def addHoliday(self,holidayObj):
+        try:
+            temp = Holiday(holidayObj.name, holidayObj.date)
+            self.innerHolidays.append(temp)
+            print(f"Successfully added holiday {holidayObj}.")
+        except TypeError:
+            print("Error: that is not a Holiday object.")
 
+    #Check date *************************************************************?
     def isValidDate(date):
-        format = "%Y-%m-d"
+        format = "%Y-%m-%d"
         x = False
         while x == False: 
             try:
@@ -46,24 +41,12 @@ class HolidayList:
                 print("Invalid date. Please try again.") 
                 date = input(str("Date (yyyy-mm-dd): "))
 
-    def addHoliday(holidayObj):
-        innerHolidays = []
-        #print("Success:")
-        innerHolidays.append(holidayObj)
-        #print(Holiday.name + "(" + Holiday.date + ") has been added to the holiday list.")
-        #print(type(innerHolidays['0']))
-        return innerHolidays
-        # Make sure holidayObj is an Holiday Object by checking the type
-        # Use innerHolidays.append(holidayObj) to add holiday
-        # print to the user that you added a holiday
-
-    def findHoliday(self):
-        find_holiday = str(input("What holiday would you like to find? "))
-        if find_holiday in HolidayList.innerHolidays:
-                Name = find_holiday
-                return HolidayList.innerHolidays[Name]
-        else:
-            print("Could not find " + find_holiday + " in the list")
+    def findHoliday(self, h):
+        for i in self.innerHolidays:
+            if i.name == h.name and i.date == h.date:
+                return i
+            
+        return None
         # Find Holiday in innerHolidays
         # Return Holiday
 
@@ -88,62 +71,85 @@ class HolidayList:
         # Read in things from json file location
         # Use addHoliday function to add holidays to inner list.
 
-    #def save_to_json(filelocation):
+    def save_to_json():
+        with open("holidayList.json", "w") as f:
+            json.dump(innerHolidays, f, indent = 1)
+
+        f.close()
         # Write out json file to selected file.
-        
+
     def scrapeHolidays():
-        holidays = []
-        for x in range(2019, 2024):
-            html = requests.get("https://www.timeanddate.com/calendar/print.html?year=" + str(x) + "&country=1&hol=33554809&holm=1&df=1").text
-            soup = BeautifulSoup(html, "html.parser")
+        years = ["2020","2021","2022","2023","2024"]
+        months = {"Jan":"01","Feb":"02","Mar":"03","Apr":"04","May":"05","Jun":"06","Jul":"07","Aug":"08","Sep":"09","Oct":"10","Nov":"11","Dec":"12"}
 
-            table = soup.find('table', attrs = {'class': 'cht lpad'})
-
-
-            for row in table.find_all('tr'):
-                cells = row.find_all('td')
-                holiday = {}
-                holiday['date'] = cells[0].string + " " + str(x)
-                holiday['name'] = cells[1].string
-                HolidayList.addHoliday(holiday)
+        for i in years:
+            url = f"https://www.timeanddate.com/holidays/us/{i}"
+            response = requests.get(url).text
+                        
+            soup = BeautifulSoup(response, 'html.parser')
+            holiday_table = soup.find('table',attrs={'id':'holidays-table'})
+            for row in holiday_table.find_all_next('tr',attrs={'class':'showrow'}):
+                date_tag = row.find('th')           #find the tag with the date in it
+                date_text = date_tag.string         #extract the raw string from the tag
+                date_month = date_text[0:2]         #extract the 3-letter month code
+                date_month = months[date_month]     #convert it to number code based on above dictionary
+                date_day = date_text[-2:].strip()   #extract the 2-digit day
+                if len(date_day) == 1:              #if day is only 1 digit (eg. '2')
+                    date_day = f"0{date_day}"       #convert it to 2-digit format (eg. '02')
+                combined_date = f"{i}-{date_month}-{date_day}"  #create formatted date
+                
+                name_tag = row.find('a')            #find the tag with the holiday name
+                name_text = name_tag.string         #extract the string from the tag
+                
+                holi = Holiday(name_text, combined_date)
+                if findHoliday(holi) == False:   #if new holiday is not in the list
+                    addHoliday(holi)            #add it to the list
+        print("Successfully scraped holiday data for 2020-2024")
         # Scrape Holidays from https://www.timeanddate.com/holidays/us/ 
         # Remember, 2 previous years, current year, and 2  years into the future. You can scrape multiple years by adding year to the timeanddate URL. For example https://www.timeanddate.com/holidays/us/2022
         # Check to see if name and date of holiday is in innerHolidays array
         # Add non-duplicates to innerHolidays
         # Handle any exceptions.     
 
+    def displayHolidaysInWeek(self):
+        for i in self.innerHolidays:
+            print(str(i))
+
     def numHolidays(self):
         return print('There are ' + str(len(HolidayList.innerHolidays)) + ' holidays in the file')
         #Return the total number of holidays in innerHolidays
         
     
-    #def filter_holidays_by_week(year, week_number):
+    def filter_holidays_by_week(year, week_number):
 
         # Use a Lambda function to filter by week number and save this as holidays, use the filter on innerHolidays
         # Week number is part of the the Datetime object
         # Cast filter results as list
         # return your holidays
+        pass
 
-    #def displayHolidaysInWeek(holidayList):
-
+    def displayHolidaysInWeek(holidayList):
+        for i in holidayList:
+            print(str(i))
         # Use your filter_holidays_by_week to get list of holidays within a week as a parameter
         # Output formated holidays in the week. 
         # * Remember to use the holiday __str__ method.
 
-    #def getWeather(weekNum):
+    def getWeather(weekNum):
         # Convert weekNum to range between two days
         # Use Try / Except to catch problems
         # Query API for weather in that week range
         # Format weather information and return weather string.
+        pass
 
-    #def viewCurrentWeek():
+    def viewCurrentWeek():
         # Use the Datetime Module to look up current week and year
         # Use your filter_holidays_by_week function to get the list of holidays 
         # for the current week/year
         # Use your displayHolidaysInWeek function to display the holidays in the week
         # Ask user if they want to get the weather
         # If yes, use your getWeather function and display results
-
+        pass
 
 
 def mainMenu():
@@ -195,8 +201,10 @@ def choice3():
             HolidayList.save_to_json()
             print("Success: ")
             print("Your changes have been saved.")
+            return
         else:
             print("Canceled")
+            return
 
 def choice4():
     print("View Holidays")
@@ -240,12 +248,15 @@ def choice5():
     print("Exit")
     print("=====")
     exit_valid = False
-    while exit_valid == False:
+    while not exit_valid:
         exit = str(input("Are you sure you want to exit? [y/n]"))
         if exit != 'y' and exit != 'n':
             print("Please only enter 'y' or 'n'")
         elif exit =='n':
-            print("You will now be directed back to the main menu")              
+            print("You will now be directed back to the main menu") 
+            exit_valid = True
+        else: 
+            exit_valid = True
     return exit
    
 
@@ -257,23 +268,23 @@ def main():
     print(" ")
     print(" ")
     HolidayList.scrapeHolidays()
-    #end_program = False
-    #while end_program == False:
-    #    choice = mainMenu()
-    #    if choice == 1:
-    #        choice1()
-    #    elif choice == 2:
-    #        choice2()
-    #    elif choice == 3:
-    #        choice3()
-    #    elif choice == 4:
-    #        choice4()
-    #    elif choice == 5:
-    #        exit = choice5()
-    #        if exit == 'y':
-    #            end_program = True
-    #    else:
-    #        print("Please only enter 1-5")
+    end_program = False
+    while end_program == False:
+        choice = mainMenu()
+        if choice == 1:
+            choice1()
+        elif choice == 2:
+            choice2()
+        elif choice == 3:
+            choice3()
+        elif choice == 4:
+            choice4()
+        elif choice == 5:
+            exit = choice5()
+            if exit == 'y':
+                end_program = True
+        else:
+            print("Please only enter 1-5")
     
     # Large Pseudo Code steps
     # -------------------------------------
